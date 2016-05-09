6160217e3d8eea61872de35b3e77d97b2d13c2aa
vicriss <412646974@qq.com>
2016-05-09 22:24:10 +0800

    remove comment of 3.py

--- 3.py
+++ 3.py
@@ -4,5 +4,5 @@ __date__ = '2016/02/12'
 import re, urllib2
 
 f = urllib2.urlopen('http://www.pythonchallenge.com/pc/def/equality.html')
-result = re.findall(r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]', f.read())   # ^[A-Z] 匹配以大写字母开头的文本串 [^A-Z] 表示与不包含大写字母的字符匹配
+result = re.findall(r'[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]', f.read())  
 print ''.join(result)
